8a217fb0f27951af32ae813986d0332337d4cbc6
SANYANG <chunyangdai@outlook.com>
2021-03-26 17:58:56 +0800

    读.pdb

--- hw05-readPdb.py
+++ hw05-readPdb.py
@@ -7,17 +7,28 @@ filename = '6xre'
 seq = ''
 lastL = 'A'
 lastN = 0
+changeM = 0
 with open(filename + '.pdb', 'r', encoding='utf-8') as fr:
     with open(filename + '.fasta', 'w', encoding='utf-8') as fw:
         fw.write('> ' + filename + ' | Chain A | ' + '\n')
         for line in fr:
             if line[0:4] == 'ATOM':
                 col = line.split()
-                if not col[5] == lastN:
+                if not col[5] == lastN:  # 检测编号是否改变
                     seq = seq + aa3_1[col[3]]
-                    if not col[4][0] == lastL:
+
+                    if not col[4][0] == lastL:  # 检测亚基是否改变，亚基变化，开新seq
                         fw.write(seq + '\n' + '> ' + filename + ' | Chain ' + col[4][0] + ' |\n')
                         seq = ''
-                lastL = col[4][0]
-                lastN = col[5]
+                        changeM = 0  # 编号从新计数了
+
+                lastL = col[4][0]  # 当编号大于999时，会出现如 A1002 这种问题
+
+                if changeM == 0:
+                    lastN = col[5]
+                    if int(col[5]) == 999:
+                        changeM = 1  # 此时编号出现如 A1002 这种问题，换方法提区编号
+                elif changeM == 1:
+                    lastN = col[4][1:]
+
         fw.write(seq + '\n')
